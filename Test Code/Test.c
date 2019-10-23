786aed89fb6a3c9475f8ef406e4c0dd5b74dedfb
Saul Hennessy <saul.heno@gmail.com>
2019-10-23 15:42:19 +0100

    Some 'roughwork'

---
+++ Test Code/Test.c
@@ -0,0 +1,18 @@
+/*
+Saul Hennessy
+XXXXXXXXXXXXXX
+Written on XX/XX/XX
+Written in Atom (Linux/Windows) and compiled with GCC
+*/
+#include <stdio.h>
+int main()
+{
+  #define Func 'Hello_Test'  //trying to get words to be easily subbed in to a file.
+
+  printf("%ls", Func);
+
+	getchar();
+
+
+	return 0;
+}
