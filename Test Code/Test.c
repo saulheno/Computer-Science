bbd33cda70f27da1992ccdfc5c7f2ff25c8036de
Saul Hennessy <saul.heno@gmail.com>
2019-10-29 13:54:26 +0000

    some more testing

--- Test Code/Test.c
+++ Test Code/Test.c
@@ -11,7 +11,7 @@ int main()
   printf("How many questions (Max of 5)?\n");
   scanf("%d", &qnum);
   printf("%d here", qnum);
-  if(qnum != 1)&&(qnum != 2)&&(qnum != 3)&&(qnum != 4)&&(qnum != 5)
+  if(qnum != 1&&qnum != 2&&qnum != 3&&qnum != 4&&qnum != 5)
   {
     printf("Between 1 and 5, please.\n");
     qnum='0';
