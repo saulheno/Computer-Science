8031036331859e878e18e2a8ff9ce2434ef04a4e
Saul Hennessy <saul.heno@gmail.com>
2019-10-29 13:14:42 +0000

    Trying out error checking

--- Test Code/Test.c
+++ Test Code/Test.c
@@ -7,9 +7,19 @@ Written in Atom (Linux/Windows) and compiled with GCC
 #include <stdio.h>
 int main()
 {
-  #define Func 'Hello_Test'  //trying to get words to be easily subbed in to a file.
-
-  printf("%ls", Func);
+  int qnum;
+  printf("How many questions (Max of 5)?\n");
+  scanf("%d", &qnum);
+  printf("%d here", qnum);
+  if(qnum != 1)&&(qnum != 2)&&(qnum != 3)&&(qnum != 4)&&(qnum != 5)
+  {
+    printf("Between 1 and 5, please.\n");
+    qnum='0';
+  }
+  else
+  {
+  printf("In else");
+  }
 
 	getchar();
 
