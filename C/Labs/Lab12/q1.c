c82af14611a53e0ff6e38b419d84f1224cfde522
saulheno <saul.heno@gmail.com>
2020-02-06 10:53:58 +0000

    Labs so far

--- C/Labs/Lab12/q1.c
+++ C/Labs/Lab12/q1.c
@@ -4,10 +4,13 @@ Written on 06/02/2020
 Written in Atom (Linux/Windows) and compiled with GCC
 */
 #include <stdio.h>
+void stars()
+{
+  printf("**********");
+}
 int main()
 {
-
-
+  stars();
 	getchar();
 	return 0;
 }
---
+++ C/Labs/Lab12/q2.c
@@ -0,0 +1,32 @@
+/*
+Saul Hennessy
+Written on 06/02/2020
+Written in Atom (Linux/Windows) and compiled with GCC
+*/
+#include <stdio.h>
+
+void function (char c, int n)
+{
+  int i;
+  for (i=0;i<n;i++)
+  {
+    printf("%c", c);
+  }
+
+}
+int main()
+{
+  int n;
+  char c;
+  printf("Character?\n");
+  scanf("%c", &c);
+
+  printf("Number?\n");
+  scanf("%d", &n);
+
+  function(c, n);
+
+
+	getchar();
+	return 0;
+}
