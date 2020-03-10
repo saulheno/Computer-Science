46d9c8ae92ce00f780323e187b1b44bed59939f4
saulheno <saul.heno@gmail.com>
2020-03-10 21:17:46 +0000

    Algorithms lab

--- C/Algos/Lab6/Lab6.c
+++ C/Algos/Lab6/Lab6.c
@@ -1,36 +1,26 @@
+/*
+Saul Hennessy
+Written on 10/03/2020
+Written in Atom (Linux/Windows) and compiled with GCC
+*/
 #include <stdio.h>
 #include <string.h>
-void printvals(char a[],int h)
+#include <time.h>
+void merge(int a[], int aux[], int lo, int mid, int hi)
 {
-/*	for (int i=0;i<h;i++)
-		printf("char %d is %c\n",i,a[i]);
-*/
-	printf ("finished string is %s",a);
-}
 
-void merge(char a[], char aux[], int lo, int mid, int hi){
+  int i,j,k;
 
-    int i,j,k;
-
-    // debug lines
-	printf ("merge lo=%d, hi=%d,mid=%d lhs=",lo,hi,mid);
-	for (i=lo;i<=mid;i++) putchar(a[i]);
-	printf (" rhs=");
-	for (i=mid+1;i<=hi;i++) putchar(a[i]);
-	printf(" - |");
-	for (i=lo;i<=hi;i++) putchar(a[i]);
-	puts ("|");
-
-	// copy a[] to aux[], work on aux[] below and copy sorted values
-	// to a[]
-	for (int k = lo; k <= hi; k++) aux[k] = a[k];
+	for (int k = lo; k <= hi; k++)
+	{
+		aux[k] = a[k];
+	}
 
 	i = lo;
 	j = mid+1;
 
-	// cycle between the lo and hi values of aux[], decide in what
-	// order to copy them to a[]
-	for (k = lo; k <= hi; k++){
+	for (k = lo; k <= hi; k++)
+	{
 		if (i > mid)
 			a[k] = aux[j++]; //J++ = do it for J then increment AFTER. ++J, increment before
 		else if (j > hi)
@@ -40,32 +30,81 @@ void merge(char a[], char aux[], int lo, int mid, int hi){
 		else
 			a[k] = aux[i++];
    }
-   // that's it, a[] now has sorted values for aux[] between lo and hi
 }
 
-void msort(char *a, char *aux, int lo, int hi)
- {
-	// base case, return when hi and lo are less than or equal.
-	if (hi <= lo) return;
+void msort(int c[], int aux[], int lo, int hi)
+{
+	if (hi <= lo)
+	{
+		return;
+	}
 
-	//debug
-	printf ("msort lo=%d, hi=%d\n",lo,hi);
-
-	// mid = (lo+hi)/2, but what about overflow?
 	int mid = lo + (hi - lo) / 2;
-	// lhs
-	msort(a, aux, lo, mid);
-	// rhs
-	msort(a, aux, mid+1, hi);
-	// merge left and right
-	merge(a, aux, lo, mid, hi);
+	// left
+	msort(c, aux, lo, mid);
+	// right
+	msort(c, aux, mid+1, hi);
+	// merge
+	merge(c, aux, lo, mid, hi);
  }
 
 int main()
 {
-	char a[] = "mergesortexample";
-	char aux[sizeof(a)];
-	printf("MergeSort - start with %s\n",a);
-	msort(a, aux, 0, strlen(a) - 1);
-	printvals(a,strlen(a));
+	int i;
+	int j=5;
+	int a[5] = {1, 3, 5, 7, 9};
+	int b[5] = {2, 4, 6, 8, 10};
+	int c[10];
+	int aux[10];
+	double time_taken;
+	double t;
+
+	t=clock();
+
+	printf("MergeSort - start with\nArray 1\n");
+
+	for(i=0; i<5; i++)
+	{
+	printf("%d ",a[i]);
+	}
+
+	printf("\nArray 2\n");
+
+	for(i=0; i<5; i++)
+	{
+	printf("%d ",b[i]);
+	}
+
+	printf("\n");
+
+	for(i=0;i<5; i++)
+	{
+		c[i] = a[i];
+		c[j] = b[i];
+		j++;
+	}
+
+	printf("Merged\n");
+
+	for(i=0; i<10; i++)
+	{
+	printf("%d ",c[i]);
+	}
+
+	printf("\n");
+
+	msort(c, aux, 0, 10 - 1);
+
+	printf("Sorted\n");
+
+	for (int i=0;i<10;i++)
+	{
+		printf("%d ",c[i]);
+	}
+
+	printf("\n");
+	
+	t=clock() - t;
+	time_taken = ((double)t)/CLOCKS_PER_SEC;
+	printf("Time takes : %lfs \n", time_taken);
 }
