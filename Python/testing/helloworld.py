b3631aea46ee3f12d519c2331b75544c56658eca
saulheno <saul.heno@gmail.com>
2020-09-22 15:01:01 +0100

    lab 2

---
+++ Python/testing/helloworld.py
@@ -0,0 +1,27 @@
+
+print("Hello World")
+int_a = 27
+int_b = 5
+int_a = 6
+
+print(int_a)  # Line 1
+print(int_b + 5)
+print(int_b)
+
+a_float = 2.5
+a_int = 7
+b_int = 6
+c_int = 2
+print(a_int / b_int)  # Line 1
+print(a_int // a_float)  # Line 2
+print(a_int % b_int)  # Line 3
+print(int(a_float))  # Line 4
+print(float(a_int))  # Line 5
+
+print(b_int ** c_int)
+
+
+w = 12
+h = 4
+area = w * h
+print("The area of a circle with width", w, "and height", h, "is", area)
